7a6cc1abfe89c3f1a355ffa628125d6d68c80d92
JohnYu2000 <junye.yu.2@gmail.com>
2024-05-09 23:52:19 -0600

    Modified jest.config.js file

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+    preset: 'ts-jset',
     transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
         '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     },
     transformIgnorePatterns: [
