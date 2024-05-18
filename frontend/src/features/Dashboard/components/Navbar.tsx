5884914c5c96e7571e3b81847f7bea8d7e3ac3d6
JohnYu2000 <junye.yu.2@gmail.com>
2024-05-17 21:13:25 -0600

    Styled the navbar of the dashboard.

--- frontend/src/features/Dashboard/components/Navbar.tsx
+++ frontend/src/features/Dashboard/components/Navbar.tsx
@@ -4,7 +4,20 @@ import './Dashboard.css';
 function Navbar() {
     return (
         <div className="contain-navbar">
-
+            <div className="navbar-logo">
+                Dashboard
+            </div>
+            <div className="searchbar">
+                <input
+                    type="text"
+                    id="search-bar"
+                    className="searchbarInput"
+                    placeholder="Search"
+                />
+            </div>
+            <div className="signout-button">
+                Sign out
+            </div>
         </div>
     )
 }
