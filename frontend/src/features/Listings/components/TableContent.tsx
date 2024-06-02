47708bb6e99f895f38e3da090e83717064b3935d
JohnYu2000 <junye.yu.2@gmail.com>
2024-06-01 22:09:03 -0600

    Listings are now displayed

--- frontend/src/features/Listings/components/TableContent.tsx
+++ frontend/src/features/Listings/components/TableContent.tsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './Listings.css';
+import useListings from './../../../hooks/useListing.tsx';
 
 function TableContent() {
+    const queryParams = useMemo(() => ({}), []);
+
+    const { listings, loading, error } = useListings(queryParams);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+
     return (
         <div className="contain-tablecontent">
             <table>
-                <tr className="header">
-                    <th>City</th>
-                    <th>Price</th>
-                    <th>Address</th>
-                    <th>Number of Beds</th>
-                    <th>Number of Baths</th>
-                    <th>Province</th>
-                    <th>Population</th>
-                    <th>Latitude</th>
-                    <th>Longitude</th>
-                    <th>Median Family Income</th>
-                </tr>
+                <thead>
+                    <tr className="header">
+                        <th>Listing ID</th>
+                        <th>City</th>
+                        <th>Price</th>
+                        <th>Address</th>
+                        <th>Number of Beds</th>
+                        <th>Number of Baths</th>
+                        <th>Province</th>
+                        <th>Population</th>
+                        <th>Latitude</th>
+                        <th>Longitude</th>
+                        <th>Median Family Income</th>
+                    </tr>    
+                </thead>
+                <tbody>
+                    {listings.map((listing) => (
+                        <tr key={listing.id}>
+                            <td>{listing.id}</td>
+                            <td>{listing.city}</td>
+                            <td>{listing.price}</td>
+                            <td>{listing.address}</td>
+                            <td>{listing.numberBeds}</td>
+                            <td>{listing.numberBaths}</td>
+                            <td>{listing.province}</td>
+                            <td>{listing.population}</td>
+                            <td>{listing.latitude}</td>
+                            <td>{listing.longitude}</td>
+                            <td>{listing.medianFamilyIncome}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     )
