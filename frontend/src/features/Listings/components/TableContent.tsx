37582883cdd67ffc245383ab3cb851daf5432e7c
JohnYu2000 <junye.yu.2@gmail.com>
2024-06-01 21:15:29 -0600

    Refactored Listing page to use table html element.

--- frontend/src/features/Listings/components/TableContent.tsx
+++ frontend/src/features/Listings/components/TableContent.tsx
@@ -4,41 +4,20 @@ import './Listings.css';
 function TableContent() {
     return (
         <div className="contain-tablecontent">
-            <div className="header">
-                <div className="columnName">
-                    City
-                </div>
-                <div className="columnName">
-                    Price
-                </div>
-                <div className="columnName">
-                    Address
-                </div>
-                <div className="columnName">
-                    Number of Beds
-                </div>
-                <div className="columnName">
-                    Number of Baths
-                </div>
-                <div className="columnName">
-                    Province
-                </div>
-                <div className="columnName">
-                    Population
-                </div>
-                <div className="columnName">
-                    Latitude
-                </div>
-                <div className="columnName">
-                    Longitude
-                </div>
-                <div className="columnName">
-                    Median Family Income
-                </div>
-            </div>
-            <div className="content">
-
-            </div>
+            <table>
+                <tr className="header">
+                    <th>City</th>
+                    <th>Price</th>
+                    <th>Address</th>
+                    <th>Number of Beds</th>
+                    <th>Number of Baths</th>
+                    <th>Province</th>
+                    <th>Population</th>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th>Median Family Income</th>
+                </tr>
+            </table>
         </div>
     )
 }
