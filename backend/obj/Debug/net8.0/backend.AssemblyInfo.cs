db9174720882bda42cc07e50864009f757aacdb4
JohnYu2000 <junye.yu.2@gmail.com>
2024-06-27 16:06:03 -0600

    Styled the add listing dialog

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c5f05404a016668f491e213a1dd7fc431f3296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad827e398be52f3fe5a771f605e096affea4d9c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
