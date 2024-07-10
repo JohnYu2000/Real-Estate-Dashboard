4db0001d967ac626fcbc2d9ba2e9ceeef4e48f70
JohnYu2000 <junye.yu.2@gmail.com>
2024-07-09 20:27:39 -0600

    Incorporated the edit listing endpoint to the frontend

--- frontend/src/features/EditListingDialog/utils/api.tsx
+++ frontend/src/features/EditListingDialog/utils/api.tsx
@@ -1,17 +1,20 @@
 import axios from './../../../hooks/axiosConfig.tsx'
 
-export const editListing = async (listingData: any) => {
+export const editListing = async (id: number, listingData: any) => {
     const token = localStorage.getItem('token');
     console.log(listingData);
-    // try {
-    //     const response = await axios.put('/api/listing', listingData, {
-    //         headers: {
-    //             'Authorization': `Bearer ${token}`,
-    //             'Content-Type': 'application/json'
-    //         }
-    //     });
-    //     return response.data;
-    // } catch (error) {
-    //     throw new Error('Failed to edit listing');
-    // }
+    try {
+        const response = await axios.put('/api/listing', listingData, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            params: {
+                id: id
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to edit listing');
+    }
 };
--- frontend/src/features/EditListingDialog/utils/handleSave.tsx
+++ frontend/src/features/EditListingDialog/utils/handleSave.tsx
@@ -2,7 +2,7 @@ import { editListing } from './api.tsx';
 
 export const handleSave = async (formData: any, closeModal: () => void) => {
     try {
-        await editListing(formData);
+        await editListing(formData.id, formData);
         closeModal();
     } catch (error) {
         console.error("Failed to edit listing.", error);
