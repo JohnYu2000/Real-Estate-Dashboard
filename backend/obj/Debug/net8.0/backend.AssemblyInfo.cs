c642bb7194b251e627dae15c264a699125802517
JohnYu2000 <junye.yu.2@gmail.com>
2024-07-10 20:47:29 -0600

    Canceling edit will reset form to default values

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2123cc8413811f59440ca4f3de444b6391faed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1a4656949d3aa51dc9967526e3634c2ee6587d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/features/EditListingDialog/utils/handleCancel.tsx
+++ frontend/src/features/EditListingDialog/utils/handleCancel.tsx
@@ -1,3 +1,18 @@
-export const handleCancel = (closeModal: () => void) => {
+export const handleCancel = (selectedRow: any, setFormData: React.Dispatch<React.SetStateAction<any>>, closeModal: () => void) => {
+    if (selectedRow) {
+        setFormData({
+            id: selectedRow.id || '',
+            city: selectedRow.city || '',
+            price: selectedRow.price || '',
+            address: selectedRow.address || '',
+            numberBeds: selectedRow.numberBeds || '',
+            numberBaths: selectedRow.numberBaths || '',
+            province: selectedRow.province || '',
+            population: selectedRow.population || '',
+            latitude: selectedRow.latitude || '',
+            longitude: selectedRow.longitude || '',
+            medianFamilyIncome: selectedRow.medianFamilyIncome || ''
+        });
+    }
     closeModal();
 };
